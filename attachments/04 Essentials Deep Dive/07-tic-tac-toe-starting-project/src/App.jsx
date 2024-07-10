b22126b65ba6c8ee77bc71a6e0759d06f5296d96
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-07-10 20:15:08 +0530

    tic tac toe game name update and save

--- attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
+++ attachments/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,22 @@
+import Player from "./components/Player.jsx";
 
 function App() {
-  
 
-  return (
-    <p>Coming soon...</p>
-  )
+
+    return (
+        <main>
+            <div id="game-container">
+                <ol id="players">
+                    <Player initialName="Player 1" symbol="X"/>
+                    <Player initialName="Player 2" symbol="O"/>
+                </ol>
+
+                GAME BOARD
+            </div>
+
+            LOGS
+        </main>
+    )
 }
 
 export default App
