6babd854b30b71196321a23d90b691b13c41e53b
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-07-19 21:28:48 +0530

    projects in project list added

--- attachments/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,13 +1,52 @@
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 import NewProject from "./components/NewProject.jsx";
 import NoProjectSelected from "./components/NoProjectSelected.jsx";
+import {useState} from "react";
 
 function App() {
+
+    const [projectsState, setProjectsState] = useState({
+        selectedProjectId: undefined,
+        projects: []
+    });
+
+    function handleStartAddProject() {
+        setProjectsState(prevProjectState => {
+            return {
+                ...prevProjectState,
+                selectedProjectId: null
+            }
+        });
+    }
+
+    function handleAddProject(projectData) {
+        setProjectsState(prevProjectState => {
+            const projectId = Math.random();
+            const newProject = {
+                ...projectData,
+                id: projectId
+            }
+
+            return {
+                ...prevProjectState,
+                selectedProjectId: undefined,
+                projects: [...prevProjectState.projects, newProject]
+            }
+        })
+    }
+
+    let content;
+
+    if (projectsState.selectedProjectId === null) {
+        content = <NewProject onAdd={handleAddProject}/>
+    } else if (projectsState.selectedProjectId === undefined) {
+        content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+    }
+
     return (
         <main className="h-screen my-8 flex gap-8">
-            <ProjectsSidebar/>
-            {/*<NewProject/>*/}
-            <NoProjectSelected/>
+            <ProjectsSidebar onStartAddProject={handleStartAddProject} projects={projectsState.projects}/>
+            {content}
         </main>
     );
 }
--- attachments/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,16 +1,29 @@
 import Input from "./Input.jsx";
+import {useRef} from "react";
 
 
-export default function NewProject() {
+export default function NewProject({onAdd}) {
+    let titleRef = useRef();
+    let descriptionRef = useRef();
+    let dueDateRef = useRef();
+
+    function handleSave() {
+        const enteredTitle = titleRef.current.value;
+        const enteredDescription = descriptionRef.current.value;
+        const enteredDueDate = dueDateRef.current.value;
+
+        onAdd({title: enteredTitle, description: enteredDescription, dueDate: enteredDueDate});
+    }
+
     return <div className="w-[35rem] mt-16">
         <menu className="flex items-center justify-end gap-4 my-4">
             <li><button className="text-stone-800 hover:text-stone-950">Cancel</button></li>
-            <li><button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
+            <li><button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button></li>
         </menu>
         <div>
-            <Input label="Title" type="text"/>
-            <Input label="Description" textArea={true}/>
-            <Input label="Due Date" type="text"/>
+            <Input ref={titleRef} label="Title" type="text"/>
+            <Input ref={descriptionRef} label="Description" textArea={true}/>
+            <Input ref={dueDateRef} label="Due Date" type="date"/>
         </div>
     </div>;
 }
