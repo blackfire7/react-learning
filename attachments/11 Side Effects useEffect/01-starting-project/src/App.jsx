687900de16be82c6ae41cdef59aac9fa2266339d
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-07-25 21:54:37 +0530

    fixed bug for escape key

--- attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
+++ attachments/11 Side Effects useEffect/01-starting-project/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
 
     return (
         <>
-            <Modal open={modelIsOpen}>
+            <Modal open={modelIsOpen} onClose={handleStopRemovePlace}>
                 <DeleteConfirmation
                     onCancel={handleStopRemovePlace}
                     onConfirm={handleRemovePlace}
