5c06498dfb1a24bd3675b3237abf6325713fedf9
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-07-27 22:08:42 +0530

    useEffect cleanup function another example

--- attachments/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
+++ attachments/11 Side Effects useEffect/01-starting-project/src/components/DeleteConfirmation.jsx
@@ -1,16 +1,26 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
+
+const TIMER = 3000;
 
 export default function DeleteConfirmation({onConfirm, onCancel}) {
+
+    const [remainingTime, setRemainingTime] = useState(TIMER);
+
+   useEffect(() => {
+       setInterval(() => {
+           setRemainingTime(prevTime => prevTime - 10);
+       }, 10);
+   }, [])
 
     useEffect(() => {
         const timer = setTimeout(() => {
             onConfirm();
-        }, 3000);
+        }, TIMER);
 
         return () => {
             clearTimeout(timer);
         }
-    }, []);
+    }, [onConfirm]);
 
     return (
         <div id="delete-confirmation">
@@ -24,6 +34,7 @@ export default function DeleteConfirmation({onConfirm, onCancel}) {
                     Yes
                 </button>
             </div>
+            <progress value={remainingTime} max={TIMER}/>
         </div>
     );
 }
