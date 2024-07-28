edacc1d4e99c501e260a6072b49d74306852c2d1
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-07-28 20:12:28 +0530

    added question timers

---
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/QuestionTimer.jsx
@@ -0,0 +1,22 @@
+import {useEffect, useState} from "react";
+
+export default function QuestionTimer({timeout, onTimeout}) {
+
+    const [remainingTime, setRemainingTime] = useState(timeout);
+
+    useEffect(() => {
+        setTimeout(onTimeout, timeout);
+    }, [timeout, onTimeout]);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setRemainingTime(prevRemainingTime => prevRemainingTime - 100);
+        }, 100);
+
+        // return () => clearInterval(interval);
+    }, []);
+
+    return (
+        <progress id="question-time" max={timeout} value={remainingTime}/>
+    );
+}
--- attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,6 +1,7 @@
 import {useState} from "react";
 import QUESTIONS from '../questions.js';
 import quizCompleteImage from '../assets/quiz-complete.png';
+import QuestionTimer from "./QuestionTimer.jsx";
 
 export default function Quiz() {
 
@@ -26,6 +27,7 @@ export default function Quiz() {
     return (
         <div id="quiz">
             <div id="question">
+                <QuestionTimer timeout={10000} onTimeout={() => handleSelectAnswer(null)}/>
                 <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
                 <ul id="answers">
                     {shuffledAnswers.map((answer) => <li key={answer} className="answer">
