eeb43506dabd6dc64a632593f4d274a6d9a70573
Sahil Sharma <sahil.sharma1912@gmail.com>
2024-08-11 17:21:05 +0530

    finished quiz with summary

--- attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,7 +1,7 @@
 import {useCallback, useState} from "react";
 import QUESTIONS from '../questions.js';
-import quizCompleteImage from '../assets/quiz-complete.png';
 import Question from "./Question.jsx";
+import Summary from "./Summary.jsx";
 
 export default function Quiz() {
 
@@ -18,10 +18,7 @@ export default function Quiz() {
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
 
     if (quizIsComplete) {
-        return <div id="summary">
-            <img src={quizCompleteImage} alt="Quiz Completed"/>
-            <h2>Quiz Completed!</h2>
-        </div>
+        return <Summary userAnswers={userAnswers}/>
     }
 
     return (
